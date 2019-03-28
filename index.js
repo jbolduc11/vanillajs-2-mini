0e6040f239670ac49bfd366c1c3332764a0aed65
Jared <jaredbolduc@yahoo.com>
2019-03-28 17:02:20 -0600

    step 2

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function addTodo(event){
-    var li = document.createElement('li')
-    li.innerText = document.getElementById("li").value;
 
+document.querySelector("form").addEventListener("submit", addTodo);
+
+function addTodo(event){
+    event.preventDefault();
+    const item = document.createElement('li')
+    item.innerText = document.getElementById("item").value;
+    
+    const button= document.createElement('button');
+    button.innerText = "x";
+    button.addEventListener("click",removeTodo);
+    item.append(button);
+    
     const list = document.querySelector("ul");
-    list.appendChild(li);
+    list.appendChild(item);
+}
+
+function removeTodo(event) {
+    event.target.parentNode.remove();
 }
